c70763399bed2411b8ae34337c264e07e11bb709
ECCNYGame <ea456bx@bhsec.bard.edu>
2025-09-27 12:22:58 -0400

    first day coding

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour,
+{
+    string variable1 = "Hello ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variable2 = "World";
+        Debug.Log(variable1+varaible2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
