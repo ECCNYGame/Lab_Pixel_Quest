dba1e5b7b4f07ab646abf628f493fda0d32f3ff0
ECCNYGame <ea456bx@bhsec.bard.edu>
2025-10-18 16:37:33 -0400

    Geo_Quest Done

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GeoController : MonoBehaviour 
+public class GeoController : MonoBehaviour
 {
     public string nextLevel = "Level_2";
     private Rigidbody2D _rigidbody2D;
     public int speed = 5;
+    private SpriteRenderer _spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-        _rigidbody2D.velocity = new Vector2 (xInput * speed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(xInput * speed, _rigidbody2D.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            _spriteRenderer.color = Color.red;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            _spriteRenderer.color = Color.blue;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            _spriteRenderer.color = Color.green;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
