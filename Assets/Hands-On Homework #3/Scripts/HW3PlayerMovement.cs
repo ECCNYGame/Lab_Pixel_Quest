a6a1589e100774f65b15e135c6a5d0385deff16f
ECCNYGame <ea456bx@bhsec.bard.edu>
2025-11-01 14:13:23 -0400

    smth

--- Assets/Hands-On Homework #3/Scripts/HW3PlayerMovement.cs
+++ Assets/Hands-On Homework #3/Scripts/HW3PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HW3PlayerMovement : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class HW3PlayerMovement : MonoBehaviour
     private float _xVelocity = 0f;
     private float _yVelocity = 0f;
     public float speed = 3;
+    public string nextLevel = "Lodge Room";
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +34,19 @@ public class HW3PlayerMovement : MonoBehaviour
             _yVelocity = Input.GetAxis(HW3Structs.Input.vertical);
         }
 
-        
-        _rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed; 
+
+        _rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed;
+    }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
     }
 }
